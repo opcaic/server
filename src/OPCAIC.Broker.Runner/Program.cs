17a824f35cc232d80d76bb79e9140745c30bba95
Radek Zikmund <r.zikmund.rz@gmail.com>
2019-03-31 17:54:06 +0200

    Add hello world example of broker and worker

---
+++ src/OPCAIC.Broker.Runner/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Core.Logging;
+using OPCAIC.Protos;
+
+namespace OPCAIC.Broker.Runner
+{
+  internal class MasterImpl : Master.MasterBase
+  {
+    // Server side handler of the SayHello RPC
+    public override async Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+      Console.WriteLine($"Received hello request from {request.ClientId}");
+      //await Task.Delay(1000);
+	    Thread.Sleep(1000); // simulate heavy load
+      Console.WriteLine($"Servicing hello request from {request.ClientId}");
+      return new HelloReply
+      {
+        Message = "Hello " + request.Name + " with id " + request.ClientId
+      };
+    }
+  }
+
+	internal class Program
+	{
+		public static int Main(string[] args)
+		{
+			if (args.Length != 2 || !int.TryParse(args[1], out int port))
+			{
+				Console.WriteLine("Usage: [host] [port]");
+				return 1;
+			}
+
+			GrpcEnvironment.SetLogger(new ConsoleLogger());
+			var server = new Server
+			{
+				Services = {Master.BindService(new MasterImpl())},
+				Ports = {new ServerPort(args[0], port, ServerCredentials.Insecure)}
+			};
+			server.Start();
+
+			Console.WriteLine($"Listening for incoming connections on {args[0]}:{port}");
+			Console.WriteLine("Press any key to stop the server...");
+			Console.ReadKey();
+
+			server.ShutdownAsync().Wait();
+			return 0;
+		}
+	}
+}
---
+++ src/OPCAIC.Worker/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Core.Logging;
+using OPCAIC.Protos;
+
+namespace OPCAIC.Worker
+{
+  class Program
+  {
+    private static async Task DoRun(int i, string host, int port)
+    {
+	    await Task.Delay(100);
+
+      Channel channel = new Channel(host, port, ChannelCredentials.Insecure);
+
+      Console.WriteLine($"Starting: {i}");
+      var client = new Master.MasterClient(channel);
+      string user = "you";
+
+      var reply = await client.SayHelloAsync(new HelloRequest { Name = user, ClientId  = i});
+	    Console.WriteLine($"Received: {reply}");
+
+      await channel.ShutdownAsync();
+    }
+
+    public static void Main(string[] args)
+    {
+      if (args.Length != 2 || !int.TryParse(args[1], out int port))
+      {
+        Console.WriteLine("usage: [server] [port]");
+        return;
+      }
+
+      GrpcEnvironment.SetLogger(new ConsoleLogger());
+
+      // spawn multiple parallel connections, simulating multiple clients
+      List<Task> tasks = new List<Task>();
+      for (int i = 0; i < 10; i++)
+      {
+        tasks.Add(DoRun(i, args[0], port));
+      }
+
+      Task.WaitAll(tasks.ToArray());
+    }
+  }
+}
