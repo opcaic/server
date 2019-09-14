859a961aa2196f0ae1a5db8912f78c00960e81ff
Radek Zikmund <r.zikmund.rz@gmail.com>
2019-09-14 22:43:47 +0200

    Created a Domain project

---
+++ src/OPCAIC.Infrastructure/Configurations/DocumentConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OPCAIC.Infrastructure.Entities;
+
+namespace OPCAIC.Infrastructure.Configurations
+{
+	public class DocumentConfiguration : IEntityTypeConfiguration<Document>
+	{
+		/// <inheritdoc />
+		public void Configure(EntityTypeBuilder<Document> builder)
+		{
+			builder.Property(e => e.Name).HasMaxLength(StringLengths.DocumentName).IsRequired();
+			builder.HasIndex(d => d.Name).IsUnique();
+		}
+	}
+}
